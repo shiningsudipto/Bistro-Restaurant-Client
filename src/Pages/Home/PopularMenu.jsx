cd1adffeab4269e6b36087871c4ada3b3b622046
Sudipto Das <shiningsudipto@gmail.com>
2023-05-23 22:30:19 +0600

    popular menu btn added

--- src/Pages/Home/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu.jsx
@@ -29,6 +29,9 @@ const PopularMenu = () => {
                         ></MenuItems>)
                     }
                 </div>
+                <div className="text-center mt-6">
+                    <button className="border-b-4 rounded-xl bg-transparent p-4 border-black uppercase">view full menu</button>
+                </div>
             </section>
         </div>
     );
--- src/Pages/Shared/MenuItems.jsx
+++ src/Pages/Shared/MenuItems.jsx
@@ -3,7 +3,7 @@ const MenuItems = ({ item }) => {
     const { image, name, price, recipe } = item;
     return (
         <div className="flex space-x-4">
-            <img className="w-[120px]" style={{ borderRadius: '0 200px 150px 200px' }} src={item?.image} alt="" />
+            <img className="w-[120px]" style={{ borderRadius: '0 200px 150px 200px' }} src={image} alt="" />
             <div>
                 <h4>{name}------------</h4>
                 <p>{recipe}</p>
